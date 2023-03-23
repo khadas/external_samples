089d431354d4247d39fc876b074d361e720057e3
jkand.huang <jkand.huang@rock-chips.com>
2023-03-23 19:51:15 +0800

    sample_demo_multi_camera_eptz: fix usage error
    Signed-off-by: jkand.huang <jkand.huang@rock-chips.com>
    Change-Id: Ifde76a669a480d55fb8989105e5e8b8bbedf5042

--- example/demo/sample_demo_multi_camera_eptz.c
+++ example/demo/sample_demo_multi_camera_eptz.c
@@ -117,7 +117,7 @@ static const struct option long_options[] = {
  ******************************************************************************/
 static void print_usage(const RK_CHAR *name) {
 	printf("usage example:\n");
-	printf("\t%s -w 1920 -h 1080 -a /etc/iqfiles/ -f 30 -o /userdata/\n", name);
+	printf("\t%s -w 1920 -h 1080 -a /etc/iqfiles/ --test_frame 30 -o /userdata/\n", name);
 #if (defined RKAIQ) && (defined UAPI2)
 	printf(
 	    "\t-a | --aiq : enable aiq with dirpath provided, eg:-a /etc/iqfiles/, \n"
